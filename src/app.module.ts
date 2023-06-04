ef2f7604b9a10a9a104c2d866605e1253e88131a
saikrishnagollapalli <sai.k.gollapalli@accenture.com>
2023-06-04 15:11:37 +0530

    Updated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StudentSchema } from './crud-ops/student.schema';
+import { StudentSchema } from './crud-ops/schema/student.schema';
+import { StudentController } from './crud-ops/student/student.controller';
+import { StudentService } from './crud-ops/student/student.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/myDev'),
     MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
